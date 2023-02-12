43ca44ac863ed69c1021e616137b54e71ca3723d
Bruno Silva Barbosa <barbosa.developer@gmail.com>
2023-02-12 09:34:44 -0300

    change express for fastify

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { FastifyAdapter } from '@nestjs/platform-fastify/adapters';
 import * as compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: false }),
+  );
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(compression());
 
-  await app.listen(3333);
+  await app.listen(3333, '0.0.0.0');
 }
 bootstrap();
